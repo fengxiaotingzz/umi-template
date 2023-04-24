5c1cc76df1458e7f1627c2e13a19b6476192327f
xiaoting feng <fengxiaotingzz@163.com>
2023-04-24 17:56:32 +0800

    更新

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from 'umi';
+import { Outlet, useOutlet } from 'umi';
 
 export default function Layout() {
   return (
     <div>
-      <Outlet />
+      <Outlet context={{a: 1}}/>
     </div>
   );
 }
