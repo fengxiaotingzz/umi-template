b3d0c3d22ed29fd08bda4e36c7b9d811bbea3b0c
xiaoting feng <fengxiaotingzz@163.com>
2024-05-27 15:56:56 +0800

    fix: 修改create

--- src/common/ajax.js
+++ src/common/ajax.js
@@ -1,43 +1,37 @@
-import {message} from 'antd'
+import { message } from 'antd';
 
 const handleResult = (res) => {
-    message.error(res?.msg)
-}
-
-export const getData = ({ url }) => {
-  return fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      if (res?.response_code === 0) {
-        return res;
-      }
-
-      return handleResult(res)
-    });
+  message.error(res?.msg);
 };
 
-export const postData = ({url, ...other}) => {
-  return fetch(url, {
+export const getData = async ({ url }) => {
+  const res = await fetch(url);
+  const res_1 = await res.json();
+  if (res_1?.response_code === 0) {
+    return res_1;
+  }
+  return handleResult(res_1);
+};
+
+export const postData = async ({ url, ...other }) => {
+  const res = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    ...other
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      if (res?.response_code === 0) {
-        return res;
-      }
-
-      return handleResult(res)
-    });
-}
+    ...other,
+  });
+  const res_1 = await res.json();
+  if (res_1?.response_code === 0) {
+    return res_1;
+  }
+  return handleResult(res_1);
+};
 
 export const request = ({ url, method = 'get', ...other }) => {
   if (method === 'get') {
     return getData({ url });
   }
 
-  return postData({ url, ...other })
+  return postData({ url, ...other });
 };
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,9 +1,9 @@
-import { Outlet, useOutlet } from 'umi';
+import { Outlet } from 'umi';
 
 export default function Layout() {
   return (
     <div>
-      <Outlet context={{a: 1}}/>
+      <Outlet context={{ a: 1 }} />
     </div>
   );
 }
